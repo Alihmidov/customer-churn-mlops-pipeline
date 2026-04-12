6ec6cec88471f47a9f9bfcc32a038dd1a84ef963
Hamidov Ali <hemidoveli893@gmail.com>
2026-04-12 11:05:07 +0400

    feat: normalize categorical features using trim and lowercase

--- sql/preprocessing.sql
+++ sql/preprocessing.sql
@@ -4,3 +4,9 @@ ALTER COLUMN "Churn" TYPE INTEGER USING ("Churn"::integer),
 ALTER COLUMN "Age" TYPE INTEGER USING ("Age"::integer),
 ALTER COLUMN "Support Calls" TYPE INTEGER USING ("Support Calls"::integer),
 ALTER COLUMN "Payment Delay" TYPE INTEGER USING ("Payment Delay"::integer);
+
+UPDATE processed_churn_train
+SET 
+    "Gender" = LOWER(TRIM("Gender")),
+    "Subscription Type" = LOWER(TRIM("Subscription Type")),
+    "Contract Length" = LOWER(TRIM("Contract Length"));
