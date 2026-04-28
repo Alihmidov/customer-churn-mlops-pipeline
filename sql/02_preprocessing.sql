85de7162d2731d08e4bc8abf5344e586553a9483
Hamidov Ali <hemidoveli893@gmail.com>
2026-04-28 22:26:43 +0400

    feat(sql): add data cleaning logic

---
+++ sql/02_preprocessing.sql
@@ -0,0 +1,16 @@
+CREATE TABLE processed_churn_train AS 
+SELECT * FROM raw_churn_train;
+
+ALTER TABLE processed_churn_train
+ALTER COLUMN "Churn" TYPE INTEGER USING ("Churn"::integer),
+ALTER COLUMN "Age" TYPE INTEGER USING ("Age"::integer),
+ALTER COLUMN "Support Calls" TYPE INTEGER USING ("Support Calls"::integer),
+ALTER COLUMN "Payment Delay" TYPE INTEGER USING ("Payment Delay"::integer);
+
+UPDATE processed_churn_train
+SET 
+    "Gender" = LOWER(TRIM("Gender")),
+    "Subscription Type" = LOWER(TRIM("Subscription Type")),
+    "Contract Length" = LOWER(TRIM("Contract Length"));
+
+DELETE FROM processed_churn_train WHERE "Gender" IS NULL;
