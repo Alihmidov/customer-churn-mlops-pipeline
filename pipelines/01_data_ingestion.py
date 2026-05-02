cea657cb8823657e15205adbc1ff3fd8f14b97e9
Hamidov Ali <hemidoveli893@gmail.com>
2026-05-02 11:35:39 +0400

    feat: ingest both train and test data into separate sql tables

--- pipelines/01_data_ingestion.py
+++ pipelines/01_data_ingestion.py
@@ -1,40 +1,21 @@
 import pandas as pd
-import os
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-
-load_dotenv()
+from utils.helpers import get_db_engine
+from config.settings import settings
 
 def ingest_data():
-    user = os.getenv('DB_USER')
-    password = os.getenv('DB_PASSWORD')
-    host = os.getenv('DB_HOST')
-    port = os.getenv('DB_PORT')
-    database = os.getenv('DB_NAME')
-    
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
-    
-    datasets = {
-        "train": "data/raw/customer_churn_dataset-training-master.csv",
-        "test": "data/raw/customer_churn_dataset-testing-master.csv"
-    }
-    
     try:
-        for mode, path in datasets.items():
-            if not os.path.exists(path):
-                print(f"File not found: {path}")
-                continue
-            
-            df = pd.read_csv(path)
-            table_name = f"raw_churn_{mode}"
-            
-            df.to_sql(table_name, engine, if_exists='replace', index=False)
-            print(f"Successfully loaded {mode} data to '{table_name}' ({len(df)} rows).")
-            
-        print("\nData ingestion process completed successfully!")
-    
+        engine = get_db_engine()
+        
+        df_train = pd.read_csv(settings.RAW_DATA_TRAINING_PATH)
+        df_train.to_sql("raw_train_data", engine, if_exists="replace", index=False)
+        print("Train data loaded.")
+
+        df_test = pd.read_csv(settings.RAW_DATA_TESTING_PATH)
+        df_test.to_sql("raw_test_data", engine, if_exists="replace", index=False)
+        print("Test data loaded.")
+
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     ingest_data()
