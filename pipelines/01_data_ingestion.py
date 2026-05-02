73c6e63c69ee5e296d74b7703d10490624fa2d06
Hamidov Ali <hemidoveli893@gmail.com>
2026-05-02 13:50:25 +0400

    save train and test datasets into separate tables

--- pipelines/01_data_ingestion.py
+++ pipelines/01_data_ingestion.py
@@ -1,21 +1,23 @@
 import pandas as pd
 from utils.helpers import get_db_engine
 from config.settings import settings
+from utils.loggers_config import logger 
 
 def ingest_data():
     try:
         engine = get_db_engine()
-        
+        logger.info("Database connection successful.") # <--- print əvəzinə logger.info
+
         df_train = pd.read_csv(settings.RAW_DATA_TRAINING_PATH)
         df_train.to_sql("raw_train_data", engine, if_exists="replace", index=False)
-        print("Train data loaded.")
+        logger.info(f"Train data loaded from {settings.RAW_DATA_TRAINING_PATH}")
 
         df_test = pd.read_csv(settings.RAW_DATA_TESTING_PATH)
         df_test.to_sql("raw_test_data", engine, if_exists="replace", index=False)
-        print("Test data loaded.")
+        logger.info(f"Test data loaded from {settings.RAW_DATA_TESTING_PATH}")
 
     except Exception as e:
-        print(f"Error: {e}")
+        logger.error(f"Error occurred during data ingestion: {e}")
 
 if __name__ == "__main__":
     ingest_data()
