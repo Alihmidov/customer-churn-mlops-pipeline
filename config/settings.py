841a022407050c1816b2b3282dfde7d41606a343
Hamidov Ali <hemidoveli893@gmail.com>
2026-05-04 00:10:50 +0400

    fix: use environment variables for data paths

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+import os
 
 class Settings(BaseSettings):
     DB_USER: str
@@ -7,8 +8,9 @@ class Settings(BaseSettings):
     DB_PORT: int
     DB_NAME: str
 
-    RAW_DATA_TRAINING_PATH: str = "data/raw/customer_churn_dataset-training-master.csv"
-    RAW_DATA_TESTING_PATH: str = "data/raw/customer_churn_dataset-testing-master.csv"
+    # os.getenv istifadə edirik ki, GitHub Actions-dakı env-i görə bilsin
+    RAW_DATA_TRAINING_PATH: str = os.getenv("RAW_DATA_TRAINING_PATH", "data/raw/customer_churn_dataset-training-master.csv")
+    RAW_DATA_TESTING_PATH: str = os.getenv("RAW_DATA_TESTING_PATH", "data/raw/customer_churn_dataset-testing-master.csv")
     PROCESSED_DATA_PATH: str = "data/processed/churn_cleaned.csv"
 
     class Config:
