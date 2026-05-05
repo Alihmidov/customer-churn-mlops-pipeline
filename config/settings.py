d4660a8546767cb1755dac13d0327563fe8026c5
Hamidov Ali <hemidoveli893@gmail.com>
2026-05-05 11:05:33 +0400

    refactor: remove unnecessary comment from settings.py file

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,6 @@ class Settings(BaseSettings):
     DB_PORT: int
     DB_NAME: str
 
-    # os.getenv istifadə edirik ki, GitHub Actions-dakı env-i görə bilsin
     RAW_DATA_TRAINING_PATH: str = os.getenv("RAW_DATA_TRAINING_PATH", "data/raw/customer_churn_dataset-training-master.csv")
     RAW_DATA_TESTING_PATH: str = os.getenv("RAW_DATA_TESTING_PATH", "data/raw/customer_churn_dataset-testing-master.csv")
     PROCESSED_DATA_PATH: str = "data/processed/churn_cleaned.csv"
