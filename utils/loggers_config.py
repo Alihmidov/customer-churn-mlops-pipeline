bb89dac6856b309605f235e6674c2accd819fad7
Hamidov Ali <hemidoveli893@gmail.com>
2026-05-08 11:19:17 +0400

    refactor: centralize logging logic using Pathlib for root directory access

--- utils/loggers_config.py
+++ utils/loggers_config.py
@@ -1,18 +1,20 @@
 import logging
 import sys 
 import os
+from pathlib import Path
 
 def setup_Logger():
     logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
     
-    log_dir = "logs"
+    base_dir = Path(__file__).resolve().parent.parent
+    log_dir = base_dir / "logs"
     os.makedirs(log_dir, exist_ok = True)
     
     logging.basicConfig(
         level=logging.INFO,
         format=logging_str,
         handlers=[
-            logging.FileHandler(os.path.join(log_dir, "running_logs.log")),
+            logging.FileHandler(log_dir / "running_logs.log"),
             logging.StreamHandler(sys.stdout)
         ]
     )
